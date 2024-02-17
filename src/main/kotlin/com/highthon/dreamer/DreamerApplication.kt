a42c70970b3f09b6af3a8888aff76ce30b535f0b
koldin108902 <alex108902@naver.com>
2024-02-17 17:05:59 +0900

    init commit

---
+++ src/main/kotlin/com/highthon/dreamer/DreamerApplication.kt
@@ -0,0 +1,11 @@
+package com.highthon.dreamer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DreamerApplication
+
+fun main(args: Array<String>) {
+	runApplication<DreamerApplication>(*args)
+}
---
+++ src/test/kotlin/com/highthon/dreamer/DreamerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.highthon.dreamer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DreamerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
