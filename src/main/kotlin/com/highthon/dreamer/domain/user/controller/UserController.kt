701deb325665b4cdc41d5352b91c77dee4d66692
koldin108902 <alex108902@naver.com>
2024-02-18 00:12:09 +0900

    Fix: Controller 어노테이션 추가

--- src/main/kotlin/com/highthon/dreamer/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/highthon/dreamer/domain/user/controller/UserController.kt
@@ -8,7 +8,11 @@ import com.highthon.dreamer.global.security.jwt.dto.TokenDto
 import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
+@RequestMapping("/u")
 class UserController(
     private val userService: UserService
 ) {
